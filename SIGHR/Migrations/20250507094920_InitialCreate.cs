e2b3a5b7f62abc72c229d89888b5d992d5410243
Bruno Passarinho <Bruno Passarinho@LAPTOP-GE9G1CHB>
2025-05-07 11:40:39 +0100

    factory

---
+++ SIGHR/Migrations/20250507094920_InitialCreate.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SIGHR.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Materiais",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materiais", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Utilizadores",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PIN = table.Column<int>(type: "int", nullable: false),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utilizadores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Encomendas",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UtilizadorId = table.Column<long>(type: "bigint", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    EstadoAtual = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Encomendas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Encomendas_Utilizadores_UtilizadorId",
+                        column: x => x.UtilizadorId,
+                        principalTable: "Utilizadores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Faltas",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UtilizadorId = table.Column<long>(type: "bigint", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataFalta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Inicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Fim = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Motivo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faltas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Faltas_Utilizadores_UtilizadorId",
+                        column: x => x.UtilizadorId,
+                        principalTable: "Utilizadores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Horarios",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UtilizadorId = table.Column<long>(type: "bigint", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HoraEntrada = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HoraSaida = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EntradaAlmoco = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SaidaAlmoco = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Horarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Horarios_Utilizadores_UtilizadorId",
+                        column: x => x.UtilizadorId,
+                        principalTable: "Utilizadores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Requisicoes",
+                columns: table => new
+                {
+                    MaterialId = table.Column<long>(type: "bigint", nullable: false),
+                    EncomendaId = table.Column<long>(type: "bigint", nullable: false),
+                    Quantidade = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requisicoes", x => new { x.MaterialId, x.EncomendaId });
+                    table.ForeignKey(
+                        name: "FK_Requisicoes_Encomendas_EncomendaId",
+                        column: x => x.EncomendaId,
+                        principalTable: "Encomendas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Requisicoes_Materiais_MaterialId",
+                        column: x => x.MaterialId,
+                        principalTable: "Materiais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Encomendas_UtilizadorId",
+                table: "Encomendas",
+                column: "UtilizadorId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Faltas_UtilizadorId",
+                table: "Faltas",
+                column: "UtilizadorId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Horarios_UtilizadorId",
+                table: "Horarios",
+                column: "UtilizadorId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requisicoes_EncomendaId",
+                table: "Requisicoes",
+                column: "EncomendaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Faltas");
+
+            migrationBuilder.DropTable(
+                name: "Horarios");
+
+            migrationBuilder.DropTable(
+                name: "Requisicoes");
+
+            migrationBuilder.DropTable(
+                name: "Encomendas");
+
+            migrationBuilder.DropTable(
+                name: "Materiais");
+
+            migrationBuilder.DropTable(
+                name: "Utilizadores");
+        }
+    }
+}
