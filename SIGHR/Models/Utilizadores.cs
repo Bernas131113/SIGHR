28e3a7dc0cee6c892cfc362e220fef2712121f01
Bernardo Alves <bernardomiguelalves34@gmail.com>
2025-05-07 12:45:30 +0100

    Models

--- SIGHR/Models/Utilizadores.cs
+++ SIGHR/Models/Utilizadores.cs
@@ -8,19 +8,19 @@
         public long Id { get; set; }
 
         [Required]
-        public string Nome { get; set; }
+        public string ?Nome { get; set; }
 
         [Required]
-        public string Username { get; set; }
+        public string ?Username { get; set; }
 
         [Required]
         public int PIN { get; set; }
 
         [Required]
-        public string Tipo { get; set; }
+        public string? Tipo { get; set; }
 
-        public ICollection<Horario> Horarios { get; set; }
-        public ICollection<Falta> Faltas { get; set; }
-        public ICollection<Encomenda> Encomendas { get; set; }
+        public ICollection<Horario>? Horarios { get; set; }
+        public ICollection<Falta> ?Faltas { get; set; }
+        public ICollection<Encomenda> ?Encomendas { get; set; }
     }
 }
